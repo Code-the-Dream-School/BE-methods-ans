2b5c1b57023f29d688b9de0e5ee0176cc4e69a75
John R McGarvey <jmcgarve@bellsouth.net>
2023-04-06 21:24:53 -0400

    Update hash_to_array.rb
    fix typo, also change so that the hash is not an optional parameter.

--- hash_to_array.rb
+++ hash_to_array.rb
@@ -1,5 +1,5 @@
 # (3) Write a program that collects 5 keys and 5 values from the user, and stores them in a hash.
-# Write a function that accepts the hash as optional parameters and prints out an array of keys and an array of values.
+# Write a function that accepts the hash as a parameter and prints out an array of keys and an array of values.
 # Call the function within your program so you know it works.
 # (Question: Can you find online information on Ruby hash methods that will help with this function?)
 # Call this program hash_to_array.rb.
@@ -19,7 +19,7 @@ end
 
 new_hash = create_hash()
 
-def print_hash(a_hash={})
+def print_hash(a_hash)
   array_of_keys = a_hash.keys
   array_of_values = a_hash.values
   puts "Array of Keys"
